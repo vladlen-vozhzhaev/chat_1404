788145698842545ae35283a942a9a4eddff84338
Vladlen <vladlen@vozhzhaev.ru>
2021-11-29 10:39:05 +0300

    Переписан способ отправки списка пользователей

--- src/server/Server.java
+++ src/server/Server.java
@@ -9,8 +9,8 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 public class Server {
+    static ArrayList<User> users = new ArrayList<>();
     public static void main(String[] args) {
-        ArrayList<User> users = new ArrayList<>();
         try {
             ServerSocket serverSocket = new ServerSocket(8179);
             System.out.println("Сервер запущен");
@@ -26,32 +26,41 @@ public class Server {
                             currentUser.getOos().writeObject("Введите имя: ");
                             String userName = currentUser.getIn().readUTF();
                             currentUser.setUserName(userName);
+                            currentUser.getOos().writeObject("Добро пожаловать на сервер");
+                            sendOnlineUsers();
                             while (true){
                                 String request = currentUser.getIn().readUTF();
-                                if(request.equals("/onlineUsers")){
-                                    System.out.println("Запрос списка пользователей от клиента "+currentUser.getUserName());
-                                    ArrayList<String> usersName = new ArrayList<>();
-                                    for (User user:users) {
-                                        usersName.add(user.getUserName());
-                                    }
-                                    currentUser.getOos().writeObject(usersName);
-                                }else{
-                                    System.out.println(currentUser.getUserName()+": "+request);
-                                    for (User user: users) {
-                                        if(currentUser.getUuid().toString().equals(user.getUuid().toString())) continue;
-                                        user.getOos().writeObject(userName+": "+request);
-                                    }
+                                System.out.println(currentUser.getUserName()+": "+request);
+                                for (User user: users) {
+                                    if(currentUser.getUuid().toString().equals(user.getUuid().toString())) continue;
+                                    user.getOos().writeObject(userName+": "+request);
                                 }
+
                             }
                         }catch (IOException exception){
                             users.remove(currentUser);
                             System.out.println(currentUser.getUserName()+" отключился");
+                            sendOnlineUsers();
                         }
                     }
                 });
                 thread.start();
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendOnlineUsers(){
+        ArrayList<String> usersName = new ArrayList<>();
+        for (User user:users) {
+            usersName.add(user.getUserName());
+        }
+        try {
+            for (User user : users){
+                user.getOos().writeObject(usersName);
+            }
+        }catch (IOException e){
             e.printStackTrace();
         }
     }
